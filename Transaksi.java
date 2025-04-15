892ef93491bdd11800ca1b20bf7923eef538d1fc
daudroi <achmaddaud42@gmail.com>
2025-04-15 12:17:35 +0700

    Penambahan tampil untuk searching pada file transaksi

--- Transaksi.java
+++ Transaksi.java
@@ -8,11 +8,21 @@ public class Transaksi {
     Transaksi(){}
 
     Transaksi(String kodeTransaksi, String namaPembeli, String tanggalPembelian,int qty, barang brg){
-        this.kodeTransaksi = kodeTransaksi;
+    this.kodeTransaksi = kodeTransaksi;
     this.namaPembeli = namaPembeli;
     this.tanggalPembelian = tanggalPembelian;
     this.qty = qty;
     this.brg = brg;
+    }
+
+    public void tampilDataTransaksi() {
+        System.out.println("Kode Transaksi  : " + kodeTransaksi);
+        System.out.println("Nama Pembeli    : " + namaPembeli);
+        System.out.println("Tanggal         : " + tanggalPembelian);
+        System.out.println("Nama Barang     : " + brg.nama);
+        System.out.println("Qty             : " + qty);
+        System.out.println("Harga           : " + brg.harga);
+        System.out.println("--------------------------------");
     }
 
     
